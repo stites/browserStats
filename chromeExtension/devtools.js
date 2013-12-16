72f9f7a91e77dae70f047ece527df82e96dd2925
Sam Stites <stites.sr@gmail.com>
2013-12-16 12:11:45 -0800

    restructured interface and added default template for a chromeExt in chromeExt

---
+++ chromeExtension/devtools.js
@@ -0,0 +1,8 @@
+chrome.devtools.panels.create(
+    "browserStatsDevTools",
+    "icon19.bmp",
+    "index.html",
+    function() {
+
+    }
+);
---
+++ chromeExtension/popup.js
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <h1>hello world</h1>
+</body>
+<script src="devtools.js"></script>
+</html>
