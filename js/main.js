2099cfb69b70637fb41916b59ba51ebf73eca0d8
Sam Stites <stites.sr@gmail.com>
2013-12-16 11:33:02 -0800

    initialized firebase, loaded with dummy data

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var fred = new Firebase('https://stites.firebaseio.com/Users/Fred');
+
+//Log me in
+fred.auth('Eo85u1MXfxVA4udvqIdjnyTYkL51Zz0AFABP962M', function(error, result) {
+  if(error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log('Authenticated successfully with payload:', result.auth);
+    console.log('Auth expires at:', new Date(result.expires * 1000));
+  }
+});
+fred.push({url:'link1', title:'title1', otherdata:"otherdata1"});
+fred.push({url:'link2', title:'title2', otherdata:"otherdata2"});
