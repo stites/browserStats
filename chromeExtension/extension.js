cbc8be7fab3d7a3041156c4823ad0a49507a2490
Sam Stites <stites.sr@gmail.com>
2013-12-16 14:14:02 -0800

    pushing to firebase - need to make the primary keys longer

--- chromeExtension/extension.js
+++ chromeExtension/extension.js
@@ -135,23 +135,38 @@ function buildTypedUrlList(divName) {
     buildPopupDom(divName, urlArray.slice(0, 10));
   };
 }
+var sendToDataBase = function () {
+
+  var microsecondsPerDay = 1000 * 60 * 60 * 24 * 1;
+  var oneDayAgo = (new Date).getTime() - microsecondsPerDay;
+
+  chrome.history.search({
+    'text': '',
+    'startTime': oneDayAgo
+  },
+  function(historyItems) {
+    for (var i = 0; i < historyItems.length; ++i) {
+      addToFirebase(historyItems[i]);
+    }
+  });
+};
 
 var addToFirebase = function (histItem) {
-  console.log(histItem);
-}
+  var fred = new Firebase('https://stites.firebaseio.com/Users/Fred');
+  //Log me in
+  fred.auth('Eo85u1MXfxVA4udvqIdjnyTYkL51Zz0AFABP962M', function(error, result) {
+    if(error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log('Authenticated successfully with payload:', result.auth);
+      console.log('Auth expires at:', new Date(result.expires * 1000));
+    }
+  });
+  console.log(~~histItem.lastVisitTime);
+  fred.child(~~histItem.lastVisitTime).set(histItem);
+};
 
 document.addEventListener('DOMContentLoaded', function () {
-  var microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-  var oneWeekAgo = (new Date).getTime() - microsecondsPerWeek;
-
   buildTypedUrlList("typedUrl_div");
-  chrome.history.search({
-      'text': '',
-      'startTime': oneWeekAgo
-    },
-    function(historyItems) {
-      for (var i = 0; i < historyItems.length; ++i) {
-        addToFirebase(historyItems[i]);
-      }
-    });
+  sendToDataBase();
 });
